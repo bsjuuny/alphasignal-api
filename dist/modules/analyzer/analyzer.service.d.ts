a9463760d759134b16fa4ea3bf9c3a1956ec1229
bsjuuny <bsjuuny@naver.com>
2026-09-12 08:18:19 +0900

    chore: stop tracking build output

--- dist/modules/analyzer/analyzer.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { AnalysisResult } from './interfaces/analysis-result.interface';
-export declare class AnalyzerService {
-    private configService;
-    private openai;
-    constructor(configService: ConfigService);
-    analyzeNews(title: string, summary: string): Promise<AnalysisResult>;
-}
